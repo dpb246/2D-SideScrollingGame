240f662c434d80d6999ec5ebd4c5a28aefe6b768
MikeXander <40724902+MikeXander@users.noreply.github.com>
2019-05-22 19:32:01 -0400

    Update Circle.java
    Circles can now be defined with or without terminal velocity

--- src/physics/Circle.java
+++ src/physics/Circle.java
@@ -2,8 +2,12 @@ package physics;
 
 public class Circle extends Shape {
     private double radius;
-    Circle(double x, double y, double radius, double mass){
-        super(x, y, mass);
+    public Circle(double x, double y, double radius, double mass, boolean isStatic){
+        super(x, y, mass, isStatic, -1.0, -1.0);
+        this.radius = radius;
+    }
+    public Circle(double x, double y, double radius, double mass, boolean isStatic, double maxVelocityX, double maxVelocityY){
+        super(x, y, mass, isStatic, maxVelocityX, maxVelocityY);
         this.radius = radius;
     }
     public double getRadius(){
