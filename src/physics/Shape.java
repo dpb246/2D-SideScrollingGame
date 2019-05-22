1aacb2af06c308818392e58b30e7c68debb176bb
MikeXander <40724902+MikeXander@users.noreply.github.com>
2019-05-22 19:09:05 -0400

    Update Shape.java

--- src/physics/Shape.java
+++ src/physics/Shape.java
@@ -1,30 +1,54 @@
 package physics;
 import main.Vector2D;
+
+import java.util.ArrayList;
+
 public class Shape {
     private Vector2D position;
     private Vector2D velocity;
+    private Vector2D terminalVelocity;
+    private ArrayList<Vector2D> forces = new ArrayList<>();
     private double mass;
-    Shape(double x, double y, double mass){
-        position = new Vector2D(x, y);
+    private boolean isStatic = false;
+    Shape(double x, double y, double mass, boolean isStatic, double maxVelocityX, double maxVelocityY){
+        this.position = new Vector2D(x, y);
         this.mass = mass;
+        this.isStatic = isStatic;
+        this.terminalVelocity = new Vector2D(maxVelocityX, maxVelocityY);
+        velocity = new Vector2D(0.0, 0.0);
     }
     public Vector2D getPosition(){
         return this.position;
     }
-    public Shape setPosition(double x, double y){
-        position.setX(x);
-        position.setY(y);
+    public Shape setPosition(Vector2D position){
+        this.position.setX(position.getX());
+        this.position.setY(position.getY());
         return this;
     }
     public Vector2D getVelocity(){
         return this.velocity;
     }
-    public Shape setVelocity(double x, double y){
-        velocity.setX(x);
-        velocity.setY(y);
+    public Shape setVelocity(Vector2D velocity){
+        this.velocity.setX(velocity.getX());
+        this.velocity.setY(velocity.getY());
         return this;
     }
     public double getMass(){
         return this.mass;
+    }
+    public Vector2D getTerminalVelocity(){
+        return this.terminalVelocity;
+    }
+    public boolean isStatic(){return this.isStatic;}
+    public Shape addForce(Vector2D force){
+        forces.add(force);
+        return this;
+    }
+    public ArrayList<Vector2D> getForces(){
+        return this.forces;
+    }
+    public Shape clearForces(){
+        forces = new ArrayList<>();
+        return this;
     }
 }
