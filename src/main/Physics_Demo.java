1b52ce14dc9bb6a90047d903d85a8d67c9ba8c75
MikeXander <40724902+MikeXander@users.noreply.github.com>
2019-05-23 11:27:50 -0400

    Physics Touch Ups 2

--- src/main/Physics_Demo.java
+++ src/main/Physics_Demo.java
@@ -4,8 +4,8 @@ import physics.*;
 public class Physics_Demo {
 
     public static void main(String[] args) {
-        PhysicsEngine world = new PhysicsEngine(9.8);
-        Box test = new Box(0.0,0.0,2.0,2.0,10.0, false, 10.0, 10.0);
+        PhysicsEngine world = new PhysicsEngine(new Vector2D(0,-9.8));
+        Box test = new Box(0.0,0.0,2.0,2.0, false, 10.0, 10.0, 10.0);
         world.add(test);
         int simCycles = 15;
         for (int cycle = 0; cycle < simCycles; cycle++){
--- src/physics/Box.java
+++ src/physics/Box.java
@@ -3,12 +3,12 @@ package physics;
 public class Box extends Shape {
     private double height;
     private double width;
-    public Box(double x, double y, double height, double width, double mass, boolean isStatic){
-        super(x, y, mass, isStatic, -1.0, -1.0);
+    public Box(double x, double y, double height, double width, boolean isStatic){
+        super(x, y, 1.0, isStatic, -1.0, -1.0);
         this.height = height;
         this.width = width;
     }
-    public Box(double x, double y, double height, double width, double mass, boolean isStatic, double maxVelocityX, double maxVelocityY){
+    public Box(double x, double y, double height, double width, boolean isStatic, double mass, double maxVelocityX, double maxVelocityY){
         super(x, y, mass, isStatic, maxVelocityX, maxVelocityY);
         this.height = height;
         this.width = width;
